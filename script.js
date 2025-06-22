535b5e94e21ca15c51a11633d9e12da37d4fb24a
David Lopes <zecastargames@gmail.com>
2025-06-22 20:05:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -205,8 +205,8 @@ document.querySelectorAll('.gallery-video').forEach(video => {
 });
 
   // Controle do menu mobile
-  const menuToggle = document.querySelector('.menu-toggle');
-  const navLinks = document.querySelector('.nav-links');
+ // const menuToggle = document.querySelector('.menu-toggle');
+ // const navLinks = document.querySelector('.nav-links');
 
   if (menuToggle && navLinks) {
     menuToggle.addEventListener('click', () => {
